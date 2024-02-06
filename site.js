2a8c248c73bbc372552e1974bdb9d196bd3bae64
Osterman <300088801@fvtc.edu>
2024-02-05 21:04:29 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,12 @@
-alert("Hello")
+//alert("Hello")
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcomeBanner = document.querySelector('#welcome p')
+if(isMorning) {welcomeBanner.textContent='Good Morning and Have a Great Day!'}
+else if (isAfternoon){welcomeBanner.textContent='Keep on Keeping on! Almost Done With The Work Day!'}
+else {welcomeBanner.textContent='Have a Great Night and Try to Relax and Unwind!'}
